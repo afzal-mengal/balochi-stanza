0d47fb57b0dadc1dfc0495f2d4c01637205624c6
afzal-mengal <afzalmengal54@gmail.com>
2026-07-02 21:40:05 +0200

    now changed to output conllu files

--- run_balochi.py
+++ run_balochi.py
@@ -1,5 +1,8 @@
 import sys
 import stanza
+from stanza.utils.conll import CoNLL
+import os
+
 
 if len(sys.argv) != 2:
     print("Usage: python run_balochi.py <text_file.txt>")
@@ -22,3 +25,14 @@ nlp = stanza.Pipeline(
 doc = nlp(text)
 
 print(doc.to_dict())
+
+doc = nlp(text)
+
+# Output filename
+output_file = os.path.splitext(input_file)[0] + ".conllu"
+
+# Write CoNLL-U file
+CoNLL.write_doc2conll(doc, output_file)
+
+print(f"CoNLL-U output written to: {output_file}")
+
